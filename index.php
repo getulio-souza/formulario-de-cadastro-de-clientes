97dc480d043d062ce69b7cf5b6af6cfa7947a9e5
Getúlio Souza <getuliosouza@live.com>
2022-03-21 11:49:20 -0300

    inserido arquivo js

--- index.php
+++ index.php
@@ -1,41 +1,20 @@
 <?php
-// if(isset($_POST["email"]) && !empty($_POST["email"]){
 
+  // definindo as variáveis
+  if(isset($_POST['create'])){
+      $nome = $_POST['nome'];
+      $sobrenome = $_POST['sobrenome'];
+      $nascimento = $_POST['nascimento'];
+      $CPF = $_POST['CPF'];
+      $telefone = $_POST['telefone'];
+      $email = $_POST['email'];
+      $endereco = $_POST['endereco'];
+      $numero = $_POST['numero'];
 
-    //definindo as variáveis
-    $nome = addslashes($_POST["nome"]);
-    $sobrenome = addslashes($_POST["sobrenome"]);
-    $nascimento = addslashes($_POST["nascimento"]);
-    $cpf = addslashes($_POST["cpf"]);
-    $telefone = addslashes($_POST["telefone"]);
-    $email = addslashes($_POST["email"]);
-    $endereço = addslashes($_POST["endereço"]);
-    $numero = addslashes($_POST["numero"]);
-    $comentario = addslashes($_POST["comentario"]);
-    
-
-//programando quem vai receber o e-mail
-$para = "getuliosouza@live.com";
-$assunto = "cadastro de clientes";
-$corpo = "nome: ".$nome. "\r\n".
-        "sobrenome: ".$sobrenome. "\r\n".
-        "email: ".$email. "\r\n".
-        "comentario: ".$comentario. "\r\n".
-        "nascimento: ".$nascimento. "\r\n".
-        "cpf: ".$cpf. "\r\n".
-        "telefone: ".$telefone. "\r\n".
-        "endereço: ".$endereço. "\r\n".
-        "numero: ".$numero. "\r\n".
-
-        $header = "From:getuliosouza8@gmail.com". "\r\n"."reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
-        //checando se o e-mail foi enviado
-        if(mail($para,$assunto,$corpo,$header)){
-            echo("Email enviado com sucesso!");
-        }
-        else{
-            echo("E-mail não pode ser enviado");
-        }
-    // }
-        
+    echo $nome - " " - $sobrenome - " " - $nascimento - " " - $CPF - " " - $telefone - " " - $email - " " - $endereco - " " - $numero;
+  }
 
 ?>
+
+
+https://www.youtube.com/watch?v=Af0X_pEt7AY&t=813s&ab_channel=ProgramadorBR
---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+//Limitando o número de caracteres do bloco de observação
+// function textLength(textarea){
+//     const textarea = document.querySelector(".text-area");
+//     if(textarea.length > 300){
+//         return "Você excedeu o máximo de caracteres permitidos"
+//     }
+// }
+
+
+//Mascara para telefone e CPF
+function criarMascara(mascaraInput){
+    const sizeInput = document.getElementById(`${mascaraInput}Input`).maxLength;
+    let valorInput = document.getElementById(`${mascaraInput}Input`).value;
+    let valorSemPonto = document.getElementById(`${mascaraInput}Input`).value.replace(/([^0-9])+/g, "")
+    const mascaras = {
+        CPF: valorInput.replace(/[^\d]/g,"").replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")};
+        
+        if(valorInput.length === sizeInput){
+            document.getElementById(`${mascaraInput}Input`).value = mascaras[mascaraInput];
+            document.getElementById(`${mascaraInput}Input`).value = valorSemPonto;
+        };
+}
+
+
+//bloquando caracteres especiais no nome 
+const blockCharFirstName = document.getElementById("firstName");
+const blockCharLastName = document.getElementById("LastName")
+
+//Nome
+blockCharFirstName.addEventListener("keypress", (e)=>{
+    if(!checkCharFirstName(e)){
+        e.preventDefault();
+    }
+});
+
+//Sobrenome
+blockCharLastName.addEventListener("keypress", (e)=>{
+ if(!checkChar(e)){
+   e.preventDefault();
+ }
+});
+
+function checkCharLastName(e){
+    const char = String.fromCharCode(e.keyCode);
+    
+    const padraoDeCaracteres = '[a-zA-z]';
+
+    if(char.match(padraoDeCaracteres)){
+        console.log(char);
+        return true;
+    }
+}
+
+
+function checkCharFirstName(e){
+    const char = String.fromCharCode(e.keyCode);
+    const padraoDeCaracteres = '[a-zA-z]';
+
+    if(char.match(padraoDeCaracteres)){
+        console.log(char);
+        return true;
+    }
+}
+
+
+// validar CPF
+
